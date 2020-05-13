261f20311134a60f41aa3d0ec00f48e2b33eca81
lenisaputri <lenisaputri98@gmail.com>
2020-05-13 12:01:40 +0700

    eak

---
+++ Assets/Script/Kode/Events2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Events2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("LVLHARD");
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
